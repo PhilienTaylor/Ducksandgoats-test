772accc94ff096b128159c31f0cf7a7196e6f49c
ducksandgoats <mrtraderjoseph@gmail.com>
2024-08-11 01:37:10 -0500

    update version

--- src/dir/init.js
+++ src/dir/init.js
@@ -1,6 +1,6 @@
 import TryGun from 'trygun'
 
-const trygun = TryGun({gun: {localStorage: false, radisk: true}, url: 'ws://retested.club:10509/announce', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', trystereo: {jsonParse: false}, debug: true})
+const trygun = TryGun({gun: {localStorage: false, radisk: true}, url: 'ws://retested.club:10509/announce', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', trystereo: {max: 1, min: 1, jsonParse: false}, debug: true})
 
 const user = trygun.user()
 
